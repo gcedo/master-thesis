2c14222f00019ed5b0c317025d7f0701f5a0d114
Edoardo Colombo <warl0ck.usenet@gmail.com>
2013-09-12 16:39:51 +0200

    Clusters added to select menu

--- visualizations/static/js/clusters.js
+++ visualizations/static/js/clusters.js
@@ -10,8 +10,8 @@ var colors = d3.scale.category20().range();
 var palette = {};
 var cluster_ids = Array();
 var vis = d3.select("#clusters-vis"),
-  width = 400,
-  height = 400,
+  width = 500,
+  height = 500,
   margins = {top: 20, right: 20, bottom: 20, left: 30},
   xMin = 0,
   xMax = 300,
@@ -30,62 +30,67 @@ var vis = d3.select("#clusters-vis"),
             .orient("left"),
   circles;
 
-  function init() {
-    d3.json("/static/data/clusters.json", function(error, json) {
-      vis.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + (height - margins.bottom) + ")")
-        .call(xAxis);
+function init() {
+  d3.json("/static/data/clusters.json", function(error, json) {
+    vis.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + (height - margins.bottom) + ")")
+      .call(xAxis);
 
-      vis.append("g")
-        .attr("class", "y axis")
-        .attr("transform", "translate(" + (margins.left) + ",0)")
-        .call(yAxis)
+    vis.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + (margins.left) + ",0)")
+      .call(yAxis)
 
-      $.each(json.domains, function(index, domain) {
-        if (cluster_ids.indexOf(domain.cluster) == -1) {
-          cluster_ids.push(domain.cluster);
-        }
-      });
+    $.each(json.domains, function(index, domain) {
+      if (cluster_ids.indexOf(domain.cluster) == -1) {
+        cluster_ids.push(domain.cluster);
+      }
+    });
 
-      $.each(cluster_ids, function(index, element) {
-        palette[element] = colors[index];
-      });
+    $.each(cluster_ids, function(index, element) {
+      palette[element] = colors[index];
+    });
 
-      update(json.domains);
+    update(json.domains);
 
-      $.each(cluster_ids, function(index, id) {
-        $("." + id).hover(
-          function() {
-            $("." + id).attr("stroke", "red");
-          },
-          function() {
-            $("." + id).attr("stroke", "none");
-          })
-      });
-      });
-  }
+    $.each(cluster_ids, function(index, id) {
+      $('#select-cluster')
+         .append($("<option></option>")
+         .attr("value", id)
+         .text(id)); 
 
-  function update(data) {
-    circles = vis.selectAll("circle")
-                   .data(data, function(d) { return d.url; });
-    circles.enter()
-      .insert("circle")
-        .attr("cx", function(d) { return xRange(+d.cf1); })
-        .attr("cy", function(d) { return yRange(+d.cf2); })
-        .attr("class", function(d) { return d.cluster; })
-        .attr("r", 2)
-        .style("fill", function(d) { return palette[d.cluster]; });
-  }
+      $("." + id).hover(
+        function() {
+          $("." + id).attr("stroke", "red");
+        },
+        function() {
+          $("." + id).attr("stroke", "none");
+        })
+    });
+    });
+}
 
-  function change_x_axis(domain) {
-    circles.transition().duration(1000).ease("exp-in-out")
-      .attr("cx", function(d) { return xRange(+d[domain])});
-  }
+function update(data) {
+  circles = vis.selectAll("circle")
+                 .data(data, function(d) { return d.url; });
+  circles.enter()
+    .insert("circle")
+      .attr("cx", function(d) { return xRange(+d.cf1); })
+      .attr("cy", function(d) { return yRange(+d.cf2); })
+      .attr("class", function(d) { return d.cluster; })
+      .attr("r", 2)
+      .style("fill", function(d) { return palette[d.cluster]; });
+}
 
-  function change_y_axis(domain) {
-    circles.transition().duration(1000).ease("exp-in-out")
-      .attr("cy", function(d) { return yRange(+d[domain])});
-  }
+function change_x_axis(domain) {
+  circles.transition().duration(1000).ease("exp-in-out")
+    .attr("cx", function(d) { return xRange(+d[domain])});
+}
 
-  init();
+function change_y_axis(domain) {
+  circles.transition().duration(1000).ease("exp-in-out")
+    .attr("cy", function(d) { return yRange(+d[domain])});
+}
+
+init();
