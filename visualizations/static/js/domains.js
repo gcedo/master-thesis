852b3f802046c7416d26c52adada094607d4c7a4
Edoardo Colombo <warl0ck.usenet@gmail.com>
2013-09-20 17:21:26 +0200

    Working on domains UI

---
+++ visualizations/static/js/domains.js
@@ -0,0 +1,18 @@
+$(function() {
+	// Slider
+	$( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+
+    // Datepicker
+    $( "#datepicker-from" ).datepicker();
+    $( "#datepicker-to" ).datepicker();
+});
