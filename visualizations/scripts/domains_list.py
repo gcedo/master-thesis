a57469863e88a4842d8d098449c53e97fd718774
Edoardo Colombo <warl0ck.usenet@gmail.com>
2013-09-26 16:19:09 +0200

    Filter works correctly

--- visualizations/scripts/domains_list.py
+++ visualizations/scripts/domains_list.py
@@ -1,14 +1,18 @@
 from flask import Response, render_template, abort, g, jsonify
 
 def render_page_content():
-	rows = g.db.webview_domains.find( {"$or" : [{"labels" : ["DGA"]}, {"labels": [] }] })
+	rows = g.db.webview_domains.find().limit(40)
 	return render_template('domains.html', rows=rows)
 
 
 def render_json_answer(parameters):
 	response_array = list()
+	query_filter = _build_query_filter(parameters)
 
-	rows = g.db.webview_domains.find(_build_query_filter(parameters))
+	if query_filter["$or"]:
+		rows = g.db.webview_domains.find(query_filter)
+	else:
+		rows = g.db.webview_domains.find()
 
 	for row in rows:
 		temp = dict()
@@ -28,11 +32,18 @@ def _build_query_filter(parameters):
 	query_filter = dict()
 	query_filter["$or"] = list()
 
+	# Check NX
+	if parameters['nx'] == 'true':
+		query_filter["$or"].append({"labels" : ["NXDOMAIN"]})
+
+	# Check DGA
 	if parameters['dga'] == 'true':
 		if parameters['nx'] == 'true':
 			query_filter["$or"].append({"labels" : ["NXDOMAIN", "DGA"]})
 		else:
 			query_filter["$or"].append({"labels" : ["DGA"]})
+
+	# Check nonDGA
 	if parameters['nonDga'] == 'true':
 		if parameters['nx'] == 'true':
 			query_filter["$or"].append({"labels" : ["NXDOMAIN"]})
