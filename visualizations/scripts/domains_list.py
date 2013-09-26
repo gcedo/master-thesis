c693ab25706287c2125f528bbd637113a01e2271
Edoardo Colombo <warl0ck.usenet@gmail.com>
2013-09-26 15:07:00 +0200

    DGA filtering json ok

--- visualizations/scripts/domains_list.py
+++ visualizations/scripts/domains_list.py
@@ -9,13 +9,15 @@ def render_page_content():
 def render_json_answer(parameters):
 	response_array = list()
 
-	if parameters['dga']:
+	if parameters['dga'] == 'true':
 		rows = g.db.webview_domains.find({ 'labels': 'DGA'})
 
 	for row in rows:
 		temp = dict()
 		temp["domain"] = row["domain"]
 		temp["labels"] = row["labels"]
+		temp["first_req_timestamp"] = row["first_req_timestamp"]
+		temp["last_req_timestamp"] = row["last_req_timestamp"]
 		response_array.append(temp)
 
 	response = dict()
