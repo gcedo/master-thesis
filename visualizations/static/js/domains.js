c1a9dbb21dcf1c4954efd05869b6d7f23f0e43d3
Edoardo Colombo <warl0ck.usenet@gmail.com>
2013-09-26 17:37:47 +0200

    Range of queries added

--- visualizations/static/js/domains.js
+++ visualizations/static/js/domains.js
@@ -1,46 +1,55 @@
 $(function() {
+
+  var SLIDER_MIN = 0, SLIDER_MAX = 3585;
+  var sliderMin = 0, sliderMax = 0;
 	// Slider
 	$( "#slider-range" ).slider({
       range: true,
-      min: 0,
-      max: 500,
-      values: [ 75, 300 ],
+      min: SLIDER_MIN,
+      max: SLIDER_MAX,
+      values: [ SLIDER_MIN, SLIDER_MAX ],
       slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        sliderMin = ui.values[0];
+        sliderMax = ui.values[1];
       }
-    });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  });
 
-    // Datepicker
-    $( "#datepicker-from" ).datepicker();
-    $( "#datepicker-to" ).datepicker();
+  $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+    " - $" + $( "#slider-range" ).slider( "values", 1 ) );
 
-    // Update page
-    $("#update-button").click(function() {
-      var dga    = "&dga=" + $("#dgaCheckbox").is(':checked');
-      var nonDga = "&nonDga=" + $("#nonDgaCheckbox").is(':checked');
-      var nx     = "&nx=" + $("#nxdomainCheckbox").is(':checked');
+  // Datepicker
+  $( "#datepicker-from" ).datepicker();
+  $( "#datepicker-to" ).datepicker();
 
-      var url = "/domains?json" + dga + nonDga + nx;
+  // Update page
+  $("#update-button").click(function() {
+    var dga     = "&dga="    + $("#dgaCheckbox").is(':checked');
+    var nonDga  = "&nonDga=" + $("#nonDgaCheckbox").is(':checked');
+    var nx      = "&nx="     + $("#nxdomainCheckbox").is(':checked');
+    var minReqs = "&minReqs=" + sliderMin;
+    var maxReqs = "&maxReqs=" + sliderMax;
 
-      $.getJSON(url, function(d) {
-        var r = new Array(), j = -1;
+    var url = "/domains?json" + dga + nonDga + nx + minReqs + maxReqs;
 
-        $.each(d["data"], function(index, element) {
-          r[++j] = '<tr><td>';
-          r[++j] = element["domain"];
-          r[++j] = '</td><td>';
-          r[++j] = element["first_req_timestamp"];
-          r[++j] = '</td><td>';
-          r[++j] = element["last_req_timestamp"];
-          r[++j] = '</td><td>';
-          r[++j] = element["labels"];
-          r[++j] = '</td></tr>';
-        });
+    alert(url);
 
-        $("#domains-list").html(r.join(''));
+    $.getJSON(url, function(d) {
+      var r = new Array(), j = -1;
 
+      $.each(d["data"], function(index, element) {
+        r[++j] = '<tr><td>';
+        r[++j] = element["domain"];
+        r[++j] = '</td><td>';
+        r[++j] = element["first_req_timestamp"];
+        r[++j] = '</td><td>';
+        r[++j] = element["last_req_timestamp"];
+        r[++j] = '</td><td>';
+        r[++j] = element["labels"];
+        r[++j] = '</td></tr>';
       });
+
+      $("#domains-list").html(r.join(''));
+
     });
+  });
 });
