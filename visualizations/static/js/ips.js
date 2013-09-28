adbd4080c689946965eb0676f01987cbc81c3104
Edoardo Colombo <warl0ck.usenet@gmail.com>
2013-09-28 17:27:32 +0200

    Dashboard checkboxes working

--- visualizations/static/js/ips.js
+++ visualizations/static/js/ips.js
@@ -1,5 +1,29 @@
 $(function() {
 
+  var countries = [];
+  var as = [];
+
+
+  // Checkboxes
+  $(".country-checkbox").change(function() {
+    var temp = $(".country-checkbox").filter(":checked");
+    countries = [];
+    $.each(temp, function(index, country) {
+      countries.push($(country).attr("id"));
+    });
+    console.log(countries);
+  });
+
+  $(".as-checkbox").change(function() {
+    var temp = $(".as-checkbox").filter(":checked");
+    as = [];
+    $.each(temp, function(index, as_name) {
+      as.push($(as_name).attr("id"));
+    });
+    console.log(as);
+  });
+
+
 // JSON download
   $("#json-download-button").click(function() {
     var url = buildUrl("json");
